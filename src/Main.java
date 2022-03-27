5ca99bb18858f494c4bc54dae7c4f2470d8cefa0
ducnguyen1206 <minhduc12037@gmail.com>
2022-03-27 20:32:32 +0700

    Updated main

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,76 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static List<String> toDos;
+    public static List<String> toDos = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String guideLine = "Please choose option: \nOption 1: add\nOption 2: delete\n" +
-                "Option 3: Modify\nPlease type your number";
-        System.out.println(guideLine);
-        String option = scanner.nextLine();
+        String stuff = "";
+        while (true) {
+            String guideLine = "\nWhat do you want to do: \n1. add\n2. delete\n" +
+                    "3. Modify\n4. Exist\nPlease type the number: ";
+            System.out.print(guideLine);
+            String option = scanner.nextLine();
 
-        switch (option) {
-            case "1":
-                System.out.println("Add your thing you want to do");
-                String toDo = scanner.nextLine();
-                System.out.println("You added " + add(toDo) + " to your todo list");
+            switch (option) {
+                case "1":
+                    System.out.print("Add your thing you want to do: ");
+                    stuff = scanner.nextLine();
+
+                    add(stuff);
+
+                    printTodoList();
+
+                    break;
+                case "2":
+                    System.out.print("Which stuff you want to delete? ");
+                    stuff = scanner.nextLine();
+
+                    delete(stuff);
+
+                    printTodoList();
+                    System.out.println();
+                    break;
+                case "3":
+                    System.out.print("Which position you want to modify? ");
+                    int position = scanner.nextInt();
+
+                    System.out.print("Change to: ");
+                    stuff = scanner.nextLine();
+
+                    modify(stuff, position);
+
+                    printTodoList();
+                    System.out.println();
+                    break;
+                default:
+                    System.out.println("Exist");
+            }
+
+            if (option.equals("4"))
                 break;
-            case "2":
-
         }
-
     }
 
-    public static String add(String toDo) {
-        toDos.add(toDo);
-        return toDo;
+    public static String add(String stuff) {
+        toDos.add(stuff);
+        return stuff;
     }
 
-    public static String delete(String toDo) {
-        return toDo;
+    public static String delete(String stuff) {
+        toDos.remove(stuff);
+        return stuff;
     }
 
-    public static String modify(String toDo, int position) {
-        return null;
+    public static void modify(String stuff, int position) {
+        toDos.set(position, stuff);
     }
 
     public static void printTodoList() {
-        int number = 1;
-        for (String toDo : toDos) {
-            System.out.println(number + ". " + toDo);
+        for (String stuff : toDos) {
+            System.out.println(stuff);
         }
     }
 }
